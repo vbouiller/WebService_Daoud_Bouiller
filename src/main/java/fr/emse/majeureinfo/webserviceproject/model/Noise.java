a32529c990d401bbc9ef679a6278146ca92a3064
vbouiller <valentin.bouiller@etu.emse.fr>
2017-11-14 11:52:35 +0100

    feat: add Noise Dao, Dto and controller need Status developped by Alaa to continue

---
+++ src/main/java/fr/emse/majeureinfo/webserviceproject/model/Noise.java
@@ -0,0 +1,52 @@
+package fr.emse.majeureinfo.webserviceproject.model;
+
+import javax.persistence.*;
+
+
+@Entity
+@SuppressWarnings("serial")
+public class Noise {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(nullable = false)
+    private Integer level;
+
+    /*@Enumerated(EnumType.STRING)
+    private Status status;*/
+
+    @SuppressWarnings("unused")
+    public Noise(){
+    }
+
+    public Noise(Integer level){//, Status status){
+        this.level=level;
+       // this.status=status;
+    }
+
+    public Long getId(){
+        return id;
+    }
+
+    public void setId(Long id){
+        this.id=id;
+    }
+
+    public Integer getLevel(){
+        return level;
+    }
+
+    public void setLevel(Integer level) {
+        this.level = level;
+    }
+
+   /* public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }*/
+}
