10fd08490599ae55ae9b2af8fc06052ebfcd4b5e
daoudalaa <daoudalaa@yahoo.co.uk>
2018-01-06 14:46:04 +0100

    feat: add mqtt publisher to switch light methodes , subscribe to some topic on project first startup

---
+++ src/main/java/fr/emse/majeureinfo/webserviceproject/mqtt/MqttThread.java
@@ -0,0 +1,34 @@
+package fr.emse.majeureinfo.webserviceproject.mqtt;
+
+import org.eclipse.paho.client.mqttv3.*;
+
+
+
+
+public class MqttThread implements  Runnable {
+    String broker="tcp://m23.cloudmqtt.com:13655";
+    String philipshue="/philipshue/";
+    MqttClient client=null;
+
+    private Thread thread;
+    private volatile boolean someCondition;
+
+    public MqttThread(){
+        this.thread = new Thread(this);
+    }
+
+    @Override
+    public void run(){
+        MqttConnector.Connection (broker,client,philipshue,"");
+        while(someCondition){
+            //doStuff();
+        }
+    }
+    public void destroy(){
+        someCondition = false;
+    }
+
+
+
+
+}
